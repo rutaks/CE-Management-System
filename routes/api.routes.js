6955c6112ae659108b453bdbcf2635d67e1d872c
rutaks <samrutakayile@awesomity.rw>
2019-11-02 11:44:08 +0200

    Added GivingCat Api Route

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,6 +1,7 @@
 import memberRoutes from "./apis/member.route";
 import fellowshipRoutes from "./apis/fellowship.route";
 import partnershipRoutes from "./apis/partnership.route";
+import givingCategoryRoutes from "./apis/giving_category.routes";
 import Router from "express";
 import Responses from "../helpers/responses";
 
@@ -14,5 +15,6 @@ router.get("/", (req, res) => {
 router.use("/fellowships", fellowshipRoutes);
 router.use("/members", memberRoutes);
 router.use("/partnerships", partnershipRoutes);
+router.use("/givings", givingCategoryRoutes);
 
 export default router;
