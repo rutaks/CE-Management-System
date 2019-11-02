ec44933966f2e526eaf727b01dd9676694da9190
rutaks <samrutakayile@awesomity.rw>
2019-11-02 11:45:47 +0200

    Added Routes

--- routes/static/partnershipPledge.route.js
+++ routes/static/partnershipPledge.route.js
@@ -4,8 +4,10 @@ import Router from "express";
 const router = Router();
 
 router.get("/", partnershipPledgeController.getAddPartnershipPledgePage);
-router.post("/", partnershipPledgeController.savePartnershipPledge);
+router.post("/partnerships", partnershipPledgeController.savePartnershipPledge);
+router.get("/partnerships", partnershipPledgeController.getPartnershipPledges);
 
-router.get("/history", partnershipPledgeController.getPartnershipPledges);
+router.post("/givings", partnershipPledgeController.saveGiving);
+router.get("/givings", partnershipPledgeController.getGivings);
 
 export default router;
