c7e558fd166004e6947d7611dff64f95eb82ded3
rutaks <samrutakayile@awesomity.rw>
2019-11-07 07:02:34 +0200

    Added Get Month Range Function

--- helpers/formatters.js
+++ helpers/formatters.js
@@ -4,6 +4,11 @@ class formatter {
   static getMomentCloudDate(date) {
     return date.utcOffset("+0700").format("YYYY-MM-DDTHH:mm:ss.SSSZ");
   }
+  static getCurrentMonthRange() {
+    const start = formatter.getMomentCloudDate(moment().startOf("month"));
+    const end = formatter.getMomentCloudDate(moment().endOf("month"));
+    return { start, end };
+  }
 }
 
 export default formatter;
