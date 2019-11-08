39a1392be77bb9ca8a8228e463146d94b1390284
rutaks <samrutakayile@awesomity.rw>
2019-11-08 07:05:37 +0200

    Added Search Partnership By Member

--- helpers/searchers.js
+++ helpers/searchers.js
@@ -7,6 +7,14 @@ class searcher {
       .populate("member")
       .populate("partnership");
   }
+  static partnershipByMember(member, start, end) {
+    return PartnershipPledge.find({
+      member: { _id: member },
+      createOn: { $gte: start, $lte: end }
+    })
+      .populate("member")
+      .populate("partnership");
+  }
 }
 
 export default searcher;
