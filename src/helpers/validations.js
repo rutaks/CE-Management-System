4bebb641d9011f185021ec8a9c1119b5a36848eb
rutaks <samrutakayile@awesomity.rw>
2019-11-16 11:53:19 +0200

    Added Fellowship Exists Validation

--- src/helpers/validations.js
+++ src/helpers/validations.js
@@ -1,10 +1,30 @@
 import Joi from "@hapi/joi";
+import Fellowship from "../models/fellowship.model";
+import Department from "../models/department.model";
+
+const fellowshipExists = fellowship => {
+  let foundFellowship = null;
+  if (fellowship.match(/^[0-9a-fA-F]{24}$/))
+    foundFellowship = Fellowship.findById(fellowship).exec();
+  return foundFellowship ? true : false;
+};
+
+const departmentExists = department => {
+  let foundDepartment = null;
+  if (department.match(/^[0-9a-fA-F]{24}$/))
+    foundDepartment = Department.findById(department).exec();
+  return foundDepartment ? true : false;
+};
 
 const validateMember = member => {
   member.phoneno = member.phoneno.replace(/-/g, "");
+  console.log();
+
   let memberSchema = Joi.object().keys({
     firstname: Joi.string().required(),
     lastname: Joi.string().required(),
+    fellowship: Joi.string().allow(""),
+    department: Joi.string().allow(""),
     email: Joi.string().email({
       minDomainSegments: 2,
       tlds: { allow: ["com", "rw", "fr"] }
@@ -14,7 +34,7 @@ const validateMember = member => {
       .valid("MALE", "FEMALE")
       .uppercase()
       .required(),
-    phonenumber: Joi.string()
+    phoneno: Joi.string()
       .trim()
       .regex(/^[0-9]{10}$/)
       .required()
@@ -22,4 +42,4 @@ const validateMember = member => {
   return memberSchema.validate(member);
 };
 
-export { validateMember };
+export { validateMember, fellowshipExists, departmentExists };
