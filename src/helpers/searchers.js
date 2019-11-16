1733050e25139da5f5c77dc96a7e1b1cba440609
rutaks <samrutakayile@awesomity.rw>
2019-11-16 21:13:41 +0200

    Added Search Option For Partnerships

--- src/helpers/searchers.js
+++ src/helpers/searchers.js
@@ -2,11 +2,20 @@ import PartnershipPledge from "../models/partnership_pledge.model";
 import formatter from "./formatters";
 
 class searcher {
-  static partnershipByDateSpan(start, end) {
-    return PartnershipPledge.find({ createOn: { $gte: start, $lte: end } })
+  static partnershipByDateSpan(start, end, partnershipArm) {
+    if (partnershipArm === "" || typeof partnershipArm === "undefined")
+      return PartnershipPledge.find({ createOn: { $gte: start, $lte: end } })
+        .populate("member")
+        .populate("partnership");
+
+    return PartnershipPledge.find({
+      partnership: partnershipArm,
+      createOn: { $gte: start, $lte: end }
+    })
       .populate("member")
       .populate("partnership");
   }
+
   static partnershipByMember(member, start, end) {
     return PartnershipPledge.find({
       member: { _id: member },
