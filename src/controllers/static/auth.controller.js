e27de8c4533a238432ab7aff6cfe15caa0e1a063
rutaks <samrutakayile@awesomity.rw>
2020-01-02 10:52:22 +0200

    Added Forgot Password Methods

--- src/controllers/static/auth.controller.js
+++ src/controllers/static/auth.controller.js
@@ -1,13 +1,126 @@
+import Member from "../../models/member.model";
 import Account from "../../models/account.model";
 import auth from "../../helpers/auth";
 import bcrypt from "bcryptjs";
-import memberController from "./member.controller";
-import Member from "../../models/member.model";
+import env from "custom-env";
+import crypto from "crypto";
+import nodemailer from "nodemailer";
 
+env.env();
 class authController {
+  static getForgotPasswordPage(req, res) {
+    res.render("auth/forgot-password", { title: "Forgot Password" });
+  }
+
+  static async forgotPassword(req, res) {
+    const { username } = req.body;
+    try {
+      const account = await Account.findOne({ username: username }).populate(
+        "member"
+      );
+      if (!account) {
+        return res.render("auth/forgot-password", {
+          errorMessage: "No Email Found",
+          oldInput: { username: username }
+        });
+      }
+      const {
+        resetPasswordToken,
+        resetPasswordExpires
+      } = generatePasswordReset();
+
+      account.resetPasswordToken = resetPasswordToken;
+      account.resetPasswordExpires = resetPasswordExpires;
+      account.save();
+
+      const host = req.headers.host;
+      const token = account.resetPasswordToken;
+      const link = `http://${host}/reset/password/${token}`;
+      const text = `Hi ${account.username} \n  Please click on the following link ${link} 
+      to reset your password. \n\n 
+      If you did not request this, please ignore this email and your password will 
+      remain unchanged.\n`;
+      const subject = "Password change request";
+      const mail = generateEmail(account.username, subject, text);
+
+      transport.sendMail(mail);
+
+      return res.render("auth/message-window", {
+        title: "Email Sent",
+        heading: "Email Succesfully sent",
+        message: "Check you email to reset your password"
+      });
+    } catch (error) {
+      console.log("ERR:", error);
+      return sendError(res, error);
+    }
+  }
+
+  static async resetPasswordPage(req, res) {
+    const token = req.params.token;
+    const account = await Account.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpires: { $gt: Date.now() }
+    });
+    if (!account) {
+      return res.render("auth/message-window", {
+        title: "Account Not Found",
+        heading: "No Account Was Found",
+        message: "Check Again your email to reset your password"
+      });
+    }
+    return res.render("auth/reset-password", { token: token });
+  }
+
+  static async resetPassword(req, res) {
+    const token = req.params.token;
+    const { password, password2 } = req.body;
+    if (password !== password2) {
+      return res.render("auth/reset-password", {
+        errorMessage: "Passwords Do Not Match",
+        token: token
+      });
+    }
+
+    try {
+      const account = await Account.findOne({
+        resetPasswordToken: token,
+        resetPasswordExpires: { $gt: Date.now() }
+      });
+      if (!account) {
+        return res.render("auth/message-window", {
+          title: "Account Not Found",
+          heading: "Invalid Token",
+          message: "Check your email to reset your password"
+        });
+      }
+      const hashedPassword = bcrypt.hashSync(password, 12);
+      account.password = hashedPassword;
+      account.resetPasswordToken = undefined;
+      account.resetPasswordExpires = undefined;
+      account.save();
+      const subject = "Your password has been changed";
+      const text = `Hi ${account.username} \n 
+                    This is a confirmation that the password for your account has just been changed.\n`;
+      const mail = generateEmail(account.username, subject, text);
+
+      transport.sendMail(mail);
+
+      return res.render("auth/message-window", {
+        title: "Password Changed",
+        heading: "Password Has Been Changed Succesfully",
+        message: `Head To The Login Page & Enter Your New Credentials`
+      });
+    } catch (error) {
+      console.log("ERR:", error);
+      return sendError(res, error);
+    }
+  }
+
   static getLoginPage(req, res) {
     res.render("auth/login", { title: "Login" });
   }
+
   static getSignupPage(req, res) {
     Member.find().then(members =>
       res.render("auth/register", {
@@ -71,5 +184,36 @@ class authController {
     });
   }
 }
+
+const transport = nodemailer.createTransport({
+  service: process.env.EMAIL_HOST,
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PWD
+  }
+});
+
+const generateEmail = (to, subject, text) => {
+  const mail = {
+    to: to,
+    from: process.env.FROM_EMAIL,
+    subject: subject,
+    text: text
+  };
+  return mail;
+};
+
+const generatePasswordReset = () => {
+  const resetPasswordToken = crypto.randomBytes(20).toString("hex");
+  const resetPasswordExpires = Date.now() + 3600000; //expires in an hour
+  return { resetPasswordToken, resetPasswordExpires };
+};
+const sendError = (res, error) => {
+  return res.render("auth/message-window", {
+    title: "404",
+    heading: "Oh No, 404. Something Has Happened",
+    message: error.message
+  });
+};
 
 export default authController;
