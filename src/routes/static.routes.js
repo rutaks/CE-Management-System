480acccb7ebf50de885c5036ca46951377dd601f
rutaks <samrutakayile@awesomity.rw>
2020-01-04 19:16:09 +0200

    Renamed Routes

--- src/routes/static.routes.js
+++ src/routes/static.routes.js
@@ -1,14 +1,13 @@
 import authRoutes from "./static/auth.route";
 import adminRoutes from "./static/admin.route";
-import partnershipPledgeRoutes from "./static/partnershipPledge.route";
+import financeRoutes from "./static/finance.route";
 import isAuth from "../middlewares/isAuth";
-
 import Router from "express";
 
 const router = Router();
 
 router.use("/", authRoutes);
-router.use("/admin", adminRoutes);
-router.use("/admin/pledges/", partnershipPledgeRoutes);
+router.use("/admin", isAuth, adminRoutes);
+router.use("/admin/finances/", isAuth, financeRoutes);
 
 export default router;
