6b5b99dca0b0eaa3bf416af81ddfbf723f087865
rutaks <samrutakayile@awesomity.rw>
2020-01-05 05:49:19 +0200

    Added Attendance Route To Api Route

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -3,12 +3,12 @@ import fellowshipRoutes from "./apis/fellowship.route";
 import departmentRoutes from "./apis/department.route";
 import partnershipRoutes from "./apis/partnership.route";
 import givingCategoryRoutes from "./apis/giving_category.routes";
+import attendanceRoutes from "./apis/attendance.route";
 import Router from "express";
 import Responses from "../helpers/responses";
 
 const router = Router();
 
-//TODO: -PLACE IN ITS OWN FUNCTION
 router.get("/", (req, res) => {
   Responses.send201(res, "Welcome To CE Management System", {});
 });
@@ -18,5 +18,5 @@ router.use("/departments", departmentRoutes);
 router.use("/members", memberRoutes);
 router.use("/partnerships", partnershipRoutes);
 router.use("/givings", givingCategoryRoutes);
-
+router.use("/attendance", attendanceRoutes);
 export default router;
